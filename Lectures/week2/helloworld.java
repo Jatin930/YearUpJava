bdbf9f95be2b17cba48deebec95a33f8ba65c36d
Jatin Talwar <jtalwar@nyc.yearup.org>
2022-11-04 19:19:28 -0400

    Commit

---
+++ Lectures/week2/helloworld.java
@@ -0,0 +1,14 @@
+public class helloworld
+    {
+
+//Hello World Program
+ 	 public static void main(String[] args)	
+    {
+     System.out.println("Hello World");
+     if(args.length>0){ 
+     System.out.println(args[0]);  
+    }
+     else System.out.println("Please enter your name");
+
+    }
+}
